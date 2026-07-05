7cb21d52bc21bf3b7ec66168b39837327c507f74
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-04 22:03:29 -0300

    feat: create validators functions

---
+++ lotomania-API/core/utils/validators.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from typing import Callable
+from django.core.exceptions import ValidationError
+
+from .parsers import parse_points
+
+__all__ = ['validate_range_points']
+
+
+def validators_decorator(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        try:
+            return func(*args, **kwargs)
+        except (AttributeError, TypeError, ValueError) as e:
+            raise ValidationError(e.message)
+    return wrapper
+
+
+@validators_decorator
+def validate_range_points(value: str | int) -> bool:
+    references = {0, 15, 16, 17, 18, 19, 20}
+    point = parse_points(value, references)
+    if point is None:
+        raise ValueError(f'Invalid value: {value}. Must be a number between 0 and 20.')
+    return True
