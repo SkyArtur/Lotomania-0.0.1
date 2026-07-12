7bc78314ab571df6271a16cbbec899e541c2821f
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-12 11:41:09 -0300

    refactor: move header and footer to main component

--- lotomania-APP/src/App.jsx
+++ lotomania-APP/src/App.jsx
@@ -1,38 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import {BrowserRouter} from "react-router";
 
-import Dashboard from './pages/Dashboard.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Main from "./components/Main.jsx";
 
 
 function App() {
     return (
-        <main className="h-auto flex flex-col items-center justify-center bg-app-image-sunshine text-gray-900 ">
+        <div className="h-auto flex flex-col items-center justify-center bg-app-image-sunshine text-gray-900 ">
             <div className="container flex flex-col items-center justify-between h-screen max-h-screen bg-mist-200">
-                <div className={"w-full ps-4 py-3 flex items-center justify-start border-b-2 border-mist-500"}>
-                    <img width={'35px'} height={'35px'} src={'/android-chrome-192x192.png'} alt={'Logo Loterias da Caixa'}/>
-                    <h1 className="ps-2 flex justify-center items-center text-4xl font-bold text-orange-500">
-                        Lotomania <sup className={'text-[.9rem] font-semibold font-mono'}>APP</sup>
-                    </h1>
-                </div>
-                <div>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={<Dashboard />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                        </Routes>
-                    </BrowserRouter>
-                </div>
-
-                <div className={"w-full ps-4 py-3 flex items-center justify-center border-b-2 border-mist-500"}>
-                    <small className={'text-xs'}>Copyright (c) 2026 Artur dos Santos Shon</small>
-                </div>
+                <BrowserRouter>
+                    <Header />
+                    <Main />
+                    <Footer />
+                </BrowserRouter>
             </div>
-
-        </main>
-
-
+        </div>
     )
 }
 
