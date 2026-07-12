621d6bddf83c4004ecda01ed91db073f7ee48ad3
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-12 00:33:14 -0300

    feat: add Register page

--- lotomania-APP/src/components/Button.jsx
+++ lotomania-APP/src/components/Button.jsx
@@ -1,7 +1,14 @@
 export default function Button({type, classes, text, ...props}) {
+    let color = 'from-blue-500 to-blue-400'
+
+    if (!type) type = 'button';
+    if (!classes) classes = '';
+    if (!text) text = 'Button';
+    if (!props) props = {};
+    if (type === 'submit') color = 'from-orange-500 to-orange-400';
+
     const classNames = `${classes} w-full rounded-lg font-bold py-1 border-2 border-mist-500 bg-linear-60 ` +
-        'from-orange-500 to-orange-400 cursor-pointer select-none transition-all ' +
-        'active:scale-[.97] hover:border-mist-600'
+        `${color} cursor-pointer select-none transition-all active:scale-[.97] hover:border-mist-600`
 
     return (
         <button className={classNames} type={type} {...props}>{text}</button>
--- lotomania-APP/src/pages/Register.jsx
+++ lotomania-APP/src/pages/Register.jsx
@@ -1,9 +1,45 @@
+import { Link, useNavigate } from "react-router";
+import { useState } from 'react';
+import { TfiReload } from "react-icons/tfi";
+
+import Input from "../components/Input.jsx";
+import Button from "../components/Button.jsx";
+
 export default function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        navigate('/')
+    }
     return (
-        <div className="container flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-semibold text-gray-100">Register</h1>
-            <h3 className={"text-xl text-gray-300"}>APP Lotomania</h3>
-            <p className="text-gray-400">Página em desenvolvimento</p>
+        <div className="max-w-180 h-full p-2 mx-auto flex flex-col items-center justify-start">
+            <div className={"w-full flex items-center justify-center"}>
+                <span className="ps-2 flex justify-center items-center text-xl h-5"></span>
+            </div>
+            <div className={'flex flex-col items-center justify-center w-full'}>
+                <div className={'max-w-100 min-w-75 sm:h-full flex flex-col items-center justify-around gap-2 py-2'}>
+                    <h3 className={'text-xl font-semibold'}>Registre seu usuário e senha</h3>
+                    <small className={'text-gray-800 text-sm text-justify italic px-2'}>
+                        Observe o limite de 15 caracteres para o campo username e o mínimo de 8 caracteres
+                        para o campo password.
+                    </small>
+                </div>
+                <div className={'min-w-75 sm:h-full'}>
+                    <form onSubmit={handleSubmit} className="w-full py-3 flex flex-col items-center justify-center gap-2">
+                        <Input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                        <div className={'w-3/4 flex flex-col items-center justify-center gap-1 py-2'}>
+                            <Input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            <Input type="password" placeholder="Confirm Password" />
+                        </div>
+                        <div className="w-full px-3 py-2 flex items-center justify-around gap-2">
+                            <Button classes={'max-w-45'} text={'Registrar'} type="submit" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
