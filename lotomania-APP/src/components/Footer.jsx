73fbf1d52f7872e90668e9ecf66039094db477b2
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-12 09:46:24 -0300

    feat: create footer component with copyright

---
+++ lotomania-APP/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className={"w-full ps-4 py-3 flex items-center justify-center border-b-2 border-mist-500"}>
+            <small className={'text-xs'}>Copyright (c) 2026 Artur dos Santos Shon</small>
+        </footer>
+    )
+}
+
+export default Footer;
