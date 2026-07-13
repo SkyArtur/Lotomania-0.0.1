c7f3f37878a6a446ef4c2c7a20859fb55a52a2af
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-12 23:53:59 -0300

    refactor: update API calls and state management

--- lotomania-APP/src/pages/Dashboard.jsx
+++ lotomania-APP/src/pages/Dashboard.jsx
@@ -1,32 +1,22 @@
-import { useEffect, useState } from 'react';
-
-import Bettor from '../components/Bettor.jsx';
-import { getMe } from '../api/getters.jsx';
+import {getLatestContest, getLatestBet, getMe} from '../api/getters.jsx';
 import { useAuth } from '../context/AuthContext.jsx';
+import {useApi} from "../hooks/useApi.js";
+import Bettor from '../components/Bettor.jsx';
+import Contest from "../components/Contest.jsx";
 
-export default function Dashboard() {
-    const [bettor, setBettor] = useState(null);
+
+function Dashboard() {
     const { token } = useAuth();
-
-    useEffect(() => {
-        let active = true;
-
-        getMe(token)
-            .then((data) => {
-                if (active) setBettor(data);
-            })
-            .catch(() => {
-                if (active) setBettor(null);
-            });
-
-        return () => {
-            active = false;
-        };
-    }, [token]);
+    const { data: bettor } = useApi(() => getMe(token), [token]);
+    const { data: contest } = useApi(() => getLatestContest(token), [token]);
+    const { data: bet } = useApi(() => getLatestBet(token), [token]);
 
     return (
         <div className="container px-2 py-1 flex flex-col items-start justify-start h-full">
             <Bettor bettor={bettor} />
+            <Contest contest={contest} />
         </div>
     )
 }
+
+export default Dashboard
