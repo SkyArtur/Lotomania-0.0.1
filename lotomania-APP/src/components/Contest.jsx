f60a5767e4faac65b0ac24e614cdec046b6bdec3
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-14 01:11:07 -0300

    refactor: add contest details

--- lotomania-APP/src/components/Contest.jsx
+++ lotomania-APP/src/components/Contest.jsx
@@ -1,51 +1,66 @@
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { useState } from "react";
 import Currency from "./Currency.jsx";
 
+
 function Contest({ contest }) {
+    if (!contest) return null
 
-    if (!contest) {
-        return null
-    }
-
+    const [showNumbers, setShowNumbers] = useState(false);
     const prizes = [...contest.prizes].sort((a, b) => b.points - a.points);
     const date = new Date(contest.date).toLocaleDateString('pt-BR');
-
     const stringNumbers = (number) => String(number).padStart(2, '0');
 
     return (
-        <div className={'max-w-100 min-w-96 flex flex-col items-center justify-center gap-1 p-3 bg-mist-100 border-2 border-mist-500 rounded-md'}>
+        <div className={'max-w-180 min-w-96 w-full flex flex-col items-center justify-center gap-1 p-3 bg-mist-100  rounded-md'}>
             <div className={'w-full flex items-center justify-between gap-1'}>
                 <div className={'relative ps-2'}>
-                    <small className={'absolute -top-2 left-0 text-xs font-mono text-mist-500'}>concurso</small>
-                    <h3 className={'text-2xl font-bold'}>{contest.reference}</h3>
+                    <small className={'absolute -top-2 left-0 text-xs font-mono text-mist-500'}>Concurso</small>
+                    <h3 className={'text-2xl font-bold text-orange-500'}><span className={'text-mist-500'}>#</span> {contest.reference}</h3>
                 </div>
                 <div className={'relative pe-2'}>
                     <small className={'absolute -top-2 -left-2 text-xs font-mono text-mist-500'}>Data</small>
-                    <h3 className={'text-xl'}>{date}</h3>
+                    <h3 className={'text-base'}>{date}</h3>
                 </div>
             </div>
             <div className={'pt-0.5 bg-mist-500 w-full inline'}></div>
-            <div className={'w-full grid grid-cols-10 gap-1'}>
-                { contest.numbers.map((number) => (
-                    <div key={number} className={'w-full h-full p-1 flex items-center justify-center text-orange-500'}>
-                        <span className={'text-xl font-semibold'}>{stringNumbers(number)}</span>
+            <div className={'w-full flex items-center justify-between px-4 gap-1'}>
+                <div className={'text-blue-900 font-semibold'}>Números Sorteados</div>
+                <button type={'button'}
+                        onClick={() => setShowNumbers(!showNumbers)}
+                        className={'cursor-pointer select-none'}>
+                    {!showNumbers
+                        ? <FaEyeSlash size={20} className={'text-red-500 hover:text-red-400'} />
+                        : <FaEye size={20} className={'text-mist-800 hover:text-mist-600'}/>
+                    }
+                </button>
+            </div>
+            <div className={'w-full max-w-100 grid grid-cols-10 gap-1'}>
+                {showNumbers &&  contest.numbers.map((number) => (
+                    <div key={number} className={'w-full p-1 flex items-center justify-center text-orange-500'}>
+                        <span className={'text-base font-semibold'}>{stringNumbers(number)}</span>
                     </div>
                 )) }
             </div>
             <div className={'pt-0.5 bg-mist-500 w-full inline'}></div>
+            <div className={'w-full flex items-center justify-between px-4 gap-1'}>
+                <div className={'text-blue-900 font-semibold'}>Premiação</div>
+            </div>
             <div className={'w-full flex flex-col items-center justify-center gap-1'}>
-                <div className={'w-full grid grid-cols-3 gap-3 font-semibold text-center border-b-2 border-mist-500'}>
-                    <div className={'flex items-center justify-center'}>Pontos</div>
-                    <div className={'flex items-center justify-center'}>Ganhadores</div>
-                    <div className={'flex items-center justify-center'}>Prêmio Pago</div>
+                <div className={'w-full max-w-100 grid grid-cols-3 gap-3 font-semibold text-center border-b-2 border-mist-500'}>
+                    <div className={'text-[.90rem] flex items-center justify-center'}>Pontos</div>
+                    <div className={'text-[.90rem] flex items-center justify-center'}>Ganhadores</div>
+                    <div className={'text-[.90rem] flex items-center justify-center'}>Prêmio Pago</div>
                 </div>
                 { prizes.map((prize) => (
-                    <div key={ prize.points } className={'w-full grid grid-cols-3 gap-3 text-sm font-mono text-center'}>
+                    <div key={ prize.points } className={'w-full max-w-100 grid grid-cols-3 gap-3 text-sm font-mono text-center'}>
                         <div className={'flex items-center justify-center'}>{ prize.points }</div>
                         <div className={'flex items-center justify-center'}>{ prize.winners }</div>
                         <div className={'flex items-center justify-start ps-3'}><Currency amount={ prize.value } /></div>
                     </div>
                 )) }
             </div>
+            <div className={'pt-0.5 bg-mist-500 w-full inline'}></div>
         </div>
     )
 }
