6b405ea649aeff80fb44e173b6b93d0e4766c912
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-14 15:16:32 -0300

    refactor: change button color and structure

--- lotomania-APP/src/components/Button.jsx
+++ lotomania-APP/src/components/Button.jsx
@@ -1,16 +1,14 @@
 export default function Button({type, classes, text, ...props}) {
-    let color = 'from-blue-500 to-blue-400'
 
     if (!type) type = 'button';
     if (!classes) classes = '';
     if (!text) text = 'Button';
     if (!props) props = {};
-    if (type === 'submit') color = 'from-orange-500 to-orange-400';
 
-    const classNames = `${classes} w-full rounded-lg font-bold py-1 border-2 border-mist-500 bg-linear-60 ` +
-        `${color} cursor-pointer select-none transition-all active:scale-[.97] hover:border-mist-600`
+    const classNames = 'w-full text-mist-100 rounded font-bold py-1 border-2 border-orange-500 bg-orange-500 ' +
+        'cursor-pointer select-none transition-all duration-300 active:scale-[.97] hover:border-orange-300 '
 
     return (
-        <button className={classNames} type={type} {...props}>{text}</button>
+        <button className={classNames + classes} type={type} {...props}>{text}</button>
     )
 }
