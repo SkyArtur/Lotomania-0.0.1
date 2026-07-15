5255d85c7a7f829c8c38b709e225c4acd6bece03
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-14 22:22:35 -0300

    refactor: remove unused logout function

--- lotomania-APP/src/components/Bettor.jsx
+++ lotomania-APP/src/components/Bettor.jsx
@@ -1,11 +1,7 @@
-import {Link} from "react-router";
-import { IoMdExit } from "react-icons/io";
 import Currency from "./Currency.jsx";
-import { useAuth } from "../context/AuthContext.jsx";
 
 
 function Bettor({bettor}) {
-    const { logout } = useAuth();
 
     if (!bettor) {
         bettor = {
@@ -23,9 +19,6 @@ function Bettor({bettor}) {
                     <small className={'text-[.7rem] font-mono text-mist-500'}>apostador #{bettor.id}</small>
                     <h3 className={'text-2xl p-0 m-0 font-bold text-blue-900'}>{bettor.username}</h3>
                 </div>
-                <Link to={"/login"} onClick={logout} className={'self-end text-xs text-mist-500 font-mono flex items-center gap-1 hover:text-red-500'}>
-                    sair <IoMdExit size={15} />
-                </Link>
             </div>
             <div className={'w-3/4 flex items-center justify-center gap-2'}>
                 <div className={'w-fit p-1 flex flex-col items-center justify-center gap-1'}>
