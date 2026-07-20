436f6347b7ad33439864b18ca6d510e9b848ae11
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-07-20 12:21:17 -0300

    refactor: add variable width prop to Input component

--- lotomania-APP/src/components/Input.jsx
+++ lotomania-APP/src/components/Input.jsx
@@ -1,6 +1,6 @@
-function Input({ type, placeholder, name, value, onChange }) {
-    const classNames = 'border-2 border-mist-500 rounded-md ps-2 py-1 bg-mist-100 w-full max-w-75 min-w-50 ' +
-        'focus:outline-1 focus:outline-orange-500'
+function Input({ type, placeholder, name, value, onChange, width }) {
+    const classNames = 'border-2 border-mist-500 rounded-md ps-2 py-1 bg-mist-100 ' +
+        'focus:outline-1 focus:outline-orange-500 ' + ( width ? width : 'w-full max-w-75 min-w-50')
 
     return (
         <input className={ classNames }
